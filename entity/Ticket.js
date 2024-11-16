4ee0a75200f3f29233d61d3086e1c3504a31ce38
zizhao <854945300@qq.com>
2024-11-17 04:10:22 +0800

    feat: ticket 添加索引

--- entity/Ticket.js
+++ entity/Ticket.js
@@ -5,6 +5,6 @@ module.exports = {
     id: {type: 'int', generated: true, primary: true},
     expiredTime: {type: 'int'}, // 过期时间
     random: {type: 'int'}, // 随机数
-    content: {type: 'varchar', unique: true}, // 内容
+    content: {type: 'varchar', unique: true, index: true}, // 内容
   }
 }
