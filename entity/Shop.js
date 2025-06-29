38d9558bbfddfb0df0807e205d141ff6f284ef23
zizhao <854945300@qq.com>
2025-06-29 22:40:46 +0800

    feat: 限制转发相关

--- entity/Shop.js
+++ entity/Shop.js
@@ -26,6 +26,7 @@ module.exports = {
     bannerCfg: {type: 'text', nullable: true},// banner 配置信息
     requiredType: {type: 'varchar', nullable: true} , // 必选分类
     typeStatus: {type: 'int', default: 0}, // 若为 1，产品分类导航栏中的‘全部’标签页将被隐藏
+    forwardPermi: {type: 'int', default: 0}, // 若为 1， 则仅管理员才可以转发
     business: {type: 'int', nullable: true},
     add_time: {type: 'int', nullable: true},
     upd_time: {type: 'int', nullable: true},
--- services/albumService.js
+++ services/albumService.js
@@ -28,7 +28,8 @@ module.exports.getShop = async (params ,cb) => {
   cond.only = [
     'id', 'desc', 'url', 'name', 'area', 'address', 'phone', 'qrcodeUrl', 'business',
     'attrs', 'specCfg', 'level', 'status', 'encry', 'waterMark', 'auditing', 'addressStatus',
-    'inveExportStatus', 'bannerStatus', 'bannerCfg', 'expiredTime', 'requiredType', 'typeStatus'
+    'inveExportStatus', 'bannerStatus', 'bannerCfg', 'expiredTime', 'requiredType', 'typeStatus',
+    'forwardPermi'
   ]
   cond.take = 100 // 限制数量
   try {
